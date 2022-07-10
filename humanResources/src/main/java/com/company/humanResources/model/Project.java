efa91a70e1c6f108c95cfa4be88eed64d48cd746
darko9899 <57688890+darko9899@users.noreply.github.com>
2022-07-10 03:55:15 +0300

    Add files via upload

---
+++ humanResources/src/main/java/com/company/humanResources/model/Project.java
@@ -0,0 +1,34 @@
+package com.company.humanResources.model;
+
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+public class Project implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ humanResources/src/main/java/com/company/humanResources/service/LoginService.java
@@ -0,0 +1,29 @@
+package com.company.humanResources.service;
+
+import com.company.humanResources.exception.ValidationCredentialsExeption;
+import com.company.humanResources.model.User;
+import com.company.humanResources.repo.UserRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class LoginService {
+    private final UserRepo userRepo;
+
+    public LoginService(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    public User login(String username, String password) {
+        Optional<User> optionalUser = userRepo.findByUsername(username);
+        User user = optionalUser.get();
+        if (user.getPassword().equals(password)) {
+            return user;
+        }
+        throw new ValidationCredentialsExeption("Невалидно потребителско име или парола.");
+    }
+
+
+}
+
